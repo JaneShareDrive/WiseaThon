d2e6689dad7f7c1ce500135a53d16581e061f8d8
JaneShareDrive <51304446+JaneShareDrive@users.noreply.github.com>
2019-06-04 14:42:59 -0400

    added comments

---
+++ app/src/main/java/com/example/wiseathon/TopicSelectionScreen.kt
@@ -0,0 +1,12 @@
+package com.example.wiseathon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TopicSelectionScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_topic_selection_screen)
+    }
+}
