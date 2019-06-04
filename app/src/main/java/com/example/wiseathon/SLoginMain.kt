2bedf92b2196be99dc4b5e5a4cd9418458219477
JaneShareDrive <51304446+JaneShareDrive@users.noreply.github.com>
2019-06-04 15:49:13 -0400

    Pre-spinner

--- app/src/main/java/com/example/wiseathon/SLoginMain.kt
+++ app/src/main/java/com/example/wiseathon/SLoginMain.kt
@@ -3,6 +3,8 @@ package com.example.wiseathon
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import org.jetbrains.anko.toast
 import com.android.volley.Request
@@ -21,20 +23,76 @@ import java.nio.file.Files.list
 
 
 class SLoginMain : AppCompatActivity() {
+    internal lateinit var sp:Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_s_login_main)
-        //Start of API Call
-        jsonParsetoObject()
 
+        // create spinner and populate with information from API
+        sp = findViewById(R.id.dropDrownTopics) as Spinner
+        val topicListsObject = retrieveTopicList()  //Call API and retrieve topic List and return list of topic Object
+        val strtopicListArray: ArrayList<String> = ArrayList()
+        for (i in 0 until topicListsObject.count())  //Converts topic object to string list for spinner selection
+        {
+            strtopicListArray.add(topicListsObject[i].topicName)
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,strtopicListArray)
+        sp.adapter = adapter
         submitTextButton.setOnClickListener {
             startActivity(Intent(this, TopicSelectionScreen::class.java))
         }
     }
 
 
-
     // function for network call
+    fun retrieveTopicList(): List<TopicClass> {
+        //replace
+        var strResp = """
+{
+"type":"Stuff",
+"Topics":[
+{
+"id":1,
+"title":"Banking"
+},
+{
+"id":2,
+"title":"Credit"
+},
+{
+"id":3,
+"title":"Personal Finance"
+},
+{
+"id":4,
+"title":"Money/Financial Planning"
+},
+{
+"id":5,
+"title":"Investments"
+}
+]
+}
+"""   //replace strResponse with API Call to get actual list
+        val jsonObj: JSONObject = JSONObject(strResp)
+        val jsonArray: JSONArray = jsonObj.getJSONArray("Topics")
+        var str_user: String = ""
+        //Create Object List of topicClass
+        val topicList = List(jsonArray.length()){TopicClass()}
+        //Fill in content from JSON Array
+        for (i in 0 until jsonArray.length()) {
+            var jsonInner: JSONObject = jsonArray.getJSONObject(i)
+            topicList[i].topicID = jsonInner.get("id").toString().toInt()
+            topicList[i].topicName = jsonInner.get("title").toString()
+        }
+        return topicList
+
+    }
+
+
+    //Example functions for use
     fun getUsers() {
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
@@ -61,13 +119,11 @@ class SLoginMain : AppCompatActivity() {
                 }
                 testText.text = "response : $str_user "
             },
-           // Response.ErrorListener { textView!!.text = "That didn't work!" })
+            // Response.ErrorListener { textView!!.text = "That didn't work!" })
             Response.ErrorListener { testText.text = "That didn't work!" })
         queue.add(stringReq)
 
     }
-
-
     fun jsonParseExample() {
         // Example for JSON Parsing
         var strResp = """
@@ -97,23 +153,22 @@ class SLoginMain : AppCompatActivity() {
        ]
     }
 """
-                val jsonObj: JSONObject = JSONObject(strResp)
-                val jsonArray: JSONArray = jsonObj.getJSONArray("Topics")
-                var str_user: String = ""
-                for (i in 0 until jsonArray.length()) {
-                    var jsonInner: JSONObject = jsonArray.getJSONObject(i)
-                    str_user = str_user + "\n" + jsonInner.get("title") /*+ questionList[1].QuestionPhrase*/
+        val jsonObj: JSONObject = JSONObject(strResp)
+        val jsonArray: JSONArray = jsonObj.getJSONArray("Topics")
+        var str_user: String = ""
+        for (i in 0 until jsonArray.length()) {
+            var jsonInner: JSONObject = jsonArray.getJSONObject(i)
+            str_user = str_user + "\n" + jsonInner.get("title") /*+ questionList[1].QuestionPhrase*/
 
-                }
-                testText.text = "response : $str_user "
+        }
+        testText.text = "response : $str_user "
     }
-
     fun jsonParsetoObject() {
         // Example for JSON Parsing to Object
 //Testing array list of objects
 
 
-var strResp = """
+        var strResp = """
 {
 "type":"Stuff",
 "Topics":[
@@ -140,21 +195,21 @@ var strResp = """
 ]
 }
 """
-val jsonObj: JSONObject = JSONObject(strResp)
-val jsonArray: JSONArray = jsonObj.getJSONArray("Topics")
-var str_user: String = ""
+        val jsonObj: JSONObject = JSONObject(strResp)
+        val jsonArray: JSONArray = jsonObj.getJSONArray("Topics")
+        var str_user: String = ""
         //Create Object List of topicClass
         val topicList = List(jsonArray.length()){TopicClass()}
         //Fill in content from JSON Array
-for (i in 0 until jsonArray.length()) {
-var jsonInner: JSONObject = jsonArray.getJSONObject(i)
-    topicList[i].topicID = jsonInner.get("id").toString().toInt()
-    topicList[i].topicName = jsonInner.get("title").toString()
-str_user = str_user + "\n" + topicList[i].topicID + "  " + topicList[i].topicName  /*+ questionList[1].QuestionPhrase*/
+        for (i in 0 until jsonArray.length()) {
+            var jsonInner: JSONObject = jsonArray.getJSONObject(i)
+            topicList[i].topicID = jsonInner.get("id").toString().toInt()
+            topicList[i].topicName = jsonInner.get("title").toString()
+            str_user = str_user + "\n" + topicList[i].topicID + "  " + topicList[i].topicName  /*+ questionList[1].QuestionPhrase*/
 
-}
-testText.text = "response : $str_user "
-}
+        }
+        testText.text = "response : $str_user "
+    }
 
 
 
