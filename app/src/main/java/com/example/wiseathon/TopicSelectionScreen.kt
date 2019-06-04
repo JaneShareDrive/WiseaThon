7e669106d4396ce697d52df2f05bd14645a3c041
JaneShareDrive <51304446+JaneShareDrive@users.noreply.github.com>
2019-06-04 16:03:47 -0400

    data screen persistence

--- app/src/main/java/com/example/wiseathon/TopicSelectionScreen.kt
+++ app/src/main/java/com/example/wiseathon/TopicSelectionScreen.kt
@@ -2,11 +2,15 @@ package com.example.wiseathon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_topic_selection_screen.*
 
 class TopicSelectionScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic_selection_screen)
+        var strTopic:String = intent.getStringExtra("Topic")
+        textView2.text = strTopic
+
     }
 }
